9a3d8056902283731437e35168adb6e9dd295ba0
Timi Ajiboye <trypio00@gmail.com>
2018-03-03 02:19:12 +0100

    chore: started bid method

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const axios = require('axios')
 async function MissionControl(needTypes, notificationURL, missionControlURL = "https://ctrl.missions.io") {
   if (!notificationURL) throw "notificationURL is not set";
   this.missionControlURL = missionControlURL;
+  this.notificationURL = notificationURL;
 
-  const registrationResponse = await axios.post(this.missionControlURL + '/needs/register', {need_types: needTypes, notification_url: notificationURL});
+  const registrationResponse = await axios.post(this.missionControlURL + '/needs/register', {need_types: needTypes, notification_url: this.notificationURL});
 
   this.davId = registrationResponse.dav_id;
-  this.notificationURL = notificationURL;
 }
 
+MissionControl.prototype.bid = async function (needId, bids){
 
-
-
+}
