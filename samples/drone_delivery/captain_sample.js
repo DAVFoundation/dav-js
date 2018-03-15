de551ef3ed388ff69c83ca4c17f6c02bb1f9eeca
Timi Ajiboye <trypio00@gmail.com>
2018-03-15 13:55:39 +0100

    chore: split working captain sample from proposed changes

--- samples/drone_delivery/captain_sample.js
+++ samples/drone_delivery/captain_sample.js
@@ -1,4 +1,4 @@
-const davJS = require('../dav-js');
+const davJS = require('../index');
 
 process.env['MISSION_CONTROL_URL'] = 'http://localhost:8888';
 process.env['NOTIFICATION_URL'] = 'https://9991eaca.ngrok.io'; // I used ngrok to point this to localhost:7000, I was having issues making requests to localhost from docker
@@ -10,95 +10,41 @@ const dav = new davJS('12345');
 const droneDelivery = dav.needs().forType('drone_delivery', {
   longitude: 3.385038,
   latitude: 6.497752,
-  radius: 10000,
-  ttl: 120 // TTL in seconds
+  radius: 10000
 });
 
 // the above line can be used to change the coordinates, it won't create multiple registrations on Mission Control. So there's no need for the .update function
 
-droneDelivery.subscibe(
-  need => onNeed(need),
-  err => console.log(err),
-  () => console.log('')
+droneDelivery.subscribe(
+  needOnNext,
+  () => console.log('completed'),
+  err => console.log(err)
 );
 
 
-function onNeed(need) {
+function needOnNext(need) {
   const bid = dav.bid().forNeed(need.id, {
     price: '13000',
     price_type: 'flat',
     price_description: 'Flat fee',
     time_to_pickup: Date.now(),
     time_to_dropoff: Date.now() + 3600000,
-    ttl: 120 // TTL in seconds
   });
   bid.subscribe(
-    onBidAccepted(bid),
+    bidOnNext,
     () => console.log('Bid completed'),
     err => console.log(err)
   );
+
+
 }
 
-function onBidAccepted(bid) {
+function bidOnNext(bid) {
   console.log(bid);
-  const contract = dav.contract().forBid(bid.id, {
-    id: '0x98782738712387623876', // Etherum Smart Contract
-    ttl: 120 // TTL in seconds
-  });
-  contract.subscribe(
-    onContractUpdated(contract),
-    () => console.log('Contract completed'),
-    err => console.log(err)
-  );
-}
 
-function onContractUpdated(contract) {
-  console.log(contract);
-  switch (contract.state) {
-    case 'signed':
-      beginMission(contract);
-      break;
-    case 'fullfilled':
-      console.log('We got some money! Hurray!');
-      break;
-  }
-}
-
-function beginMission(contract) {
-  const mission = dav.mission().begin(contract.id, {
-    id: '0x98782738712387623876', // Etherum Smart Contract
-    ttl: 120 // TTL in seconds
-  });
-  mission.subscribe(
-    onMissionUpdated(mission),
-    () => console.log('Mission completed'),
-    err => console.log(err)
-  );
-}
-
-function onMissionUpdated(mission) {
-  let drone = {/* This is the drone API - not part of DAV-JS SDK */ };
-  switch (mission.state) {
-    case 'started':
-      drone.goto(mission.pickup.lat, mission.pickup.lng)
-        .then((pos) => {
-          mission.update({ state: 'atPickup', lat: pos.lat, lng: pos.lng });
-        });
-      drone.updatesPosition.then(pos => {
-        mission.update({ state: 'movingToPickup', lat: pos.lat, lng: pos.lng });
-      });
-      break;
-    case 'packageReady':
-      drone.goto(mission.dropoff.lat, mission.dropoff.lng)
-        .then((pos) => {
-          mission.update({ state: 'atDropoff', lat: pos.lat, lng: pos.lng });
-        });
-      drone.updatesPosition.then(pos => {
-        mission.update({ state: 'movingToDropoff', lat: pos.lat, lng: pos.lng });
-      });
-      break;
-    case 'done':
-      drone.standBy();
-      break;
+  if (bid.stage === 'awarded') {
+    console.log('Yay, we have been awarded');
+  } else if (bid.stage == 'signed') {
+    // do stuff
   }
 }
