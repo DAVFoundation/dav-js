9c0974740902e710e5822f57f7c748022b26497e
srfrnk <srfrnk@gmail.com>
2018-03-25 18:41:57 +0300

    chore: removing server listening from captains

--- src/dav.js
+++ src/dav.js
@@ -7,7 +7,7 @@ function davJS(davId) {
   if (!process.env.MISSION_CONTROL_URL) throw new Error('MISSION_CONTROL_URL is not set');
   if (!process.env.NOTIFICATION_URL) throw new Error('NOTIFICATION_URL is not set');
 
-  const port = process.env.WEB_SERVER_PORT || 7000;
+  // const port = process.env.WEB_SERVER_PORT || 7000;
 
   this.davId = davId;
 
@@ -22,7 +22,9 @@ function davJS(davId) {
   this.contracts = {};
   this.missions = {};
 
-  this.server.post('/', (req, res) => {
+  setInterval(this.getUpdate.bind(this), 1000);
+
+  /* this.server.post('/', (req, res) => {
     const params = req.body;
     switch (params.notification_type) {
       case 'new_need':
@@ -44,16 +46,16 @@ function davJS(davId) {
         break;
     }
     res.sendStatus(200);
-  });
+  }); */
 
-  this.server.listen(port, function () {
-    console.log(`Listening on port ${port}`);
-  });
+  /*   this.server.listen(port, function () {
+      console.log(`Listening on port ${port}`);
+    }); */
 
-  axios.post(`${this.missionControlURL}/captains`, { dav_id: davId, notification_url: this.notificationURL })
-    .catch((err) => {
-      console.error(err);
-    });
+  /*  axios.post(`${this.missionControlURL}/captains`, { dav_id: davId, notification_url: this.notificationURL })
+     .catch((err) => {
+       console.error(err);
+     }); */
 }
 
 const generateMissionUpdateFunction = function (mission, davContext) {
@@ -63,6 +65,19 @@ const generateMissionUpdateFunction = function (mission, davContext) {
         davContext.missions[mission.bid_id].onNext(response.data);
       });
   };
+};
+
+davJS.prototype.getUpdate = function () {
+  axios.get(`${this.missionControlURL}/needs/${this.davId}`, {})
+    .then(({ data }) => {
+      data.forEach(need => {
+        this.this.needTypes[need.need_type].onNext(need);
+      });
+      console.log(data);
+    })
+    .catch(e =>
+      console.log(e)
+    );
 };
 
 davJS.prototype.needs = function () {
