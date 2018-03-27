fa4e6171945937e3179f521019f393c4de7f49bc
srfrnk <srfrnk@gmail.com>
2018-03-27 20:57:43 +0300

    flow

--- src/dav.js
+++ src/dav.js
@@ -1,6 +1,4 @@
 const axios = require('axios');
-// const express = require('express');
-// const bodyParser = require('body-parser');
 const rx = require('rx-lite');
 const web3 = require('./web3wrapper');
 const davContracts = require('./dav-contracts');
@@ -9,15 +7,11 @@ const davContracts = require('./dav-contracts');
 function davJS(davId, wallet) {
   if (!process.env.MISSION_CONTROL_URL) throw new Error('MISSION_CONTROL_URL is not set');
 
-  // const port = process.env.WEB_SERVER_PORT || 7000;
-
   this.davId = davId;
   this.wallet = wallet;
 
   this.missionControlURL = process.env.MISSION_CONTROL_URL;
 
-  // this.server = express();
-  // this.server.use(bodyParser.json());
   this.needTypes = {};
   this.bids = {};
   this.contracts = {};
@@ -30,33 +24,6 @@ function davJS(davId, wallet) {
       console.error(err);
     });
 
-  /* this.server.post('/', (req, res) => {
-    const params = req.body;
-    switch (params.notification_type) {
-      case 'new_need':
-        var need = params.data.need;
-        this.needTypes[need.need_type].onNext(need);
-        break;
-      case 'bid':
-        var bid = params.data.bid;
-        this.bids[bid.need_id].onNext(bid);
-        break;
-      case 'contract':
-        var contract = params.data.contract;
-        this.contracts[contract.bid_id].onNext(contract);
-        break;
-      case 'mission':
-        var mission = params.data.mission;
-        mission.update = generateMissionUpdateFunction(mission, this);
-        this.missions[mission.bid_id].onNext(mission);
-        break;
-    }
-    res.sendStatus(200);
-  }); */
-
-  /*   this.listener = this.server.listen(port, function () {
-      console.log(`Listening on port ${port}`);
-    }); */
 }
 
 davJS.prototype.isRegistered = function () {
@@ -95,8 +62,8 @@ davJS.prototype.connect = function () {
 
           return identityContractInstance
             .register(dav.davId, v, r, s, { from: dav.wallet })
-            .then(function (res) {
-              console.log(res);
+            .then(function (/* res */) {
+              // console.log(res);
               resolve({});
             })
             .catch(function (err) {
@@ -111,36 +78,30 @@ davJS.prototype.connect = function () {
   });
 };
 
-const generateMissionUpdateFunction = function (mission, davContext) {
-  return function ({ status, latitude, longitude }) {
-    axios.put(`${davContext.missionControlURL}/missions/${mission.mission_id}`, { status, latitude, longitude, dav_id: davContext.davId })
-      .then((response) => {
-        davContext.missions[mission.bid_id].onNext(response.data);
-      });
-  };
-};
-
 davJS.prototype.getUpdate = function () {
   let dav = this;
   axios.get(`${this.missionControlURL}/needs/${this.davId}`, {})
     .then(({ data }) => {
       data.forEach(need => {
-        dav.needTypes[need.need_type].onNext(need);
+        if (!dav.bids[need.id]) {
+          dav.needTypes[need.need_type].onNext(need);
+        }
       });
-      console.log(data);
     })
     .catch(e =>
-      console.log(e)
+      console.error(e)
     );
   axios.get(`${this.missionControlURL}/bids/${this.davId}/chosen`, {})
     .then(({ data }) => {
-      data.forEach(bid => {
-        dav.bids[bid.need_id].onNext(bid);
+      data.forEach(bidid => {
+        let bid = dav.bids[bidid];
+        if (bid) {
+          bid.onNext(bidid);
+        }
       });
-      console.log(data);
     })
     .catch(e =>
-      console.log(e)
+      console.error(e)
     );
 };
 
@@ -175,15 +136,14 @@ davJS.prototype.bid = function () {
   let dav = this;
   return {
     forNeed: (needId, bid) => {
-      dav.bids[needId] = new rx.Subject;
-      bid.dav_id = dav.davId;
-      axios.post(`${dav.missionControlURL}/bids/${needId}`, bid)
-        .then((/* response */) => {
-          // dav.bids[needId].onNext(response.data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+      if (!dav.bids[needId]) {
+        dav.bids[needId] = new rx.Subject;
+        bid.dav_id = dav.davId;
+        axios.post(`${dav.missionControlURL}/bids/${needId}`, bid)
+          .catch((err) => {
+            console.error(err);
+          });
+      }
       return dav.bids[needId];
     }
   };
@@ -237,10 +197,17 @@ davJS.prototype.mission = function () {
     begin: (bidId, missionParams) => {
       dav.missions[bidId] = new rx.Subject;
       missionParams.dav_id = dav.davId;
+      missionParams.bid_id = bidId;
       axios.post(`${dav.missionControlURL}/missions/${bidId}`, missionParams)
         .then((response) => {
           const mission = response.data;
-          mission.update = generateMissionUpdateFunction(mission, this);
+          mission.update = () => {
+            axios.put(`${dav.missionControlURL}/missions/${mission.mission_id}`,
+              { status, latitude: 1, longitude: 1, dav_id: dav.davId })
+              .then((response) => {
+                dav.missions[mission.bid_id].onNext(response.data);
+              });
+          };
           dav.missions[bidId].onNext(mission);
         }).catch((err) => {
           console.error(err);
