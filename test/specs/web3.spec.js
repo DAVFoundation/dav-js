eecd65d1c5fb2f4929667d323bf86265c320c303
Anatoly T <antt001@gmail.com>
2018-04-25 19:13:22 +0300

    fix: mocks for test

--- test/specs/web3.spec.js
+++ test/specs/web3.spec.js
@@ -5,7 +5,36 @@ jest.doMock('axios', () => {
   };
 });
 
-const { DavSDK } = require('dav-js');
+jest.doMock('web3', () => { // eslint-disable-line no-unused-vars
+  return class {};
+});
+
+jest.doMock('truffle-hdwallet-provider', () => { // eslint-disable-line no-unused-vars
+  return class {};
+});
+
+jest.doMock('truffle-contract', () => { // eslint-disable-line no-unused-vars
+  return (contract) => { // eslint-disable-line no-unused-vars
+    let instance = {};
+    for(let i in contract.abi) {
+      if(contract.abi[i].type === 'function') {
+        let resp = {};
+        for(let o in contract.abi[i].outputs) {
+          if(contract.abi[i].outputs[o].name === '' && contract.abi[i].outputs[o].type === 'bool') {
+            resp = false;
+          }
+        }
+        instance[contract.abi[i].name] = () => Promise.resolve(resp);
+      }
+    }
+    return {
+      setProvider: (provider) => Promise.resolve({ }), // eslint-disable-line no-unused-vars
+      deployed: () => Promise.resolve(instance) // eslint-disable-line no-unused-vars
+    };
+  };
+});
+
+const { DavSDK } = require('../../src/index.js');
 const mnemonic = require('./mnemonic');
 
 
@@ -16,7 +45,7 @@ describe('contract integration', () => {
     process.env.ETH_NODE_URL = 'https://ropsten.infura.io/wUiZtmeZ1KwjFrcC8zRO';
   });
 
-  test('test user register flow', async () => {
+  test('test user is register', async () => {
     expect.assertions(1);
     const sdk = new DavSDK('0x4fFCe6A06CB4317a295acaDdbb3141c73158f954', '0x4fFCe6A06CB4317a295acaDdbb3141c73158f954', mnemonic);
     await expect(sdk.isRegistered()).resolves.toEqual(false);
