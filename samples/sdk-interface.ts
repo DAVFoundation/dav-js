178cae877d868f0e13a2167e11af27b8b19a1a0a
srfrnk <srfrnk@gmail.com>
2018-07-08 20:47:12 +0300

    fix: added all requirements from samples

--- samples/sdk-interface.ts
+++ samples/sdk-interface.ts
@@ -1,7 +1,9 @@
 namespace Rx { export class Subject<T> { } } // Dummy Definition
-class BigInteger { } // Dummy Definition
 
 namespace DAV {
+    type ID = string;
+    type BigInteger = string;
+
     interface NeedTypeParamas {
         lat: number
         long: number
@@ -11,8 +13,11 @@ namespace DAV {
 
     enum PriceType { flat = 'flat' }
 
-    interface BidParams {
+    interface BasicParams {
         ttl: number // Bid TTL in seconds:
+    }
+
+    interface BidParams extends BasicParams {
         price: {
             price: BigInteger // price in Vinci
             price_type: PriceType
@@ -24,31 +29,78 @@ namespace DAV {
         }
     }
 
+    enum VehicleTypes {
+        drone = 'drone'
+    }
+
+    interface NeedParams extends BasicParams {
+        start_at: number,
+        start_latitude: number,
+        start_longitude: number,
+        end_latitude: number,
+        end_longitude: number,
+        vehicle_type: VehicleTypes,
+        max_altitude: number,
+    }
+
     export class Config {
 
     }
 
-    export class SDKFactory {
+    export function SDKFactory(config: Config = new Config()): SDK {
+        return new SDK(config)
+    }
+
+    export class SDK {
         constructor(config: Config = new Config()) { }
-        static getIdentity(davId: string, privateKey: string): Identity { return new Identity('', ''); }
+
+        //Not sure this need a config param
+        getIdentity(davId: ID, privateKey: string, config?: Config): Identity { return new Identity('', ''); }
+        async isRegistered(ID): Promise<boolean> { return false; }
+        async registerIdentity(davId: ID, walletAddress: string, walletPrivateKey: string, identityPrivateKey: string) { }
     }
 
     export class Identity {
-        constructor(davId: string, privateKey: string) { }
+        constructor(davId: ID, privateKey: string) { }
+
+        davId: ID
+
         needs(): Needs { return new Needs(); }
-        bid(bidId: string): Bid { return new Bid(); }
+        need(needId: ID): Need { return new Need(needId); }
+        bid(bidId: ID): Bid { return new Bid(bidId); }
+        mission(missionId: ID): Mission { return new Mission(missionId); }
         messages(): Rx.Subject<Message> { return new Rx.Subject<Message>(); }
+        publishNeed(needType: NeedType, params: NeedParams): Need { return new Need(''); }
+    }
+
+    enum NeedType {
+
     }
 
     export class Needs {
-        forType(needType: string, params: object): Rx.Subject<Need> { return new Rx.Subject<Need>(); }
+        forType(needType: NeedType, params: object): Rx.Subject<Need> { return new Rx.Subject<Need>(); }
     }
 
     export class Need {
-        createBid(params: BidParams): Bid { return new Bid(); }
+        constructor(needId: ID) { }
+
+        id: ID
+
+        createBid(params: BidParams): Bid { return new Bid(params); }
+        bids(): Rx.Subject<Bid> { return new Rx.Subject<Bid>(); }
     }
 
     export class Bid {
+        constructor(bidId: ID)
+        constructor(params: BidParams)
+        constructor(data?: ID | BidParams) { }
+
+        id: ID
+        priceType: PriceType
+        price: number
+
+        accept() { }
+        async signContract(walletPrivateKey: string): Promise<Mission> { return new Mission(''); }
         messages(): Rx.Subject<Message> { return new Rx.Subject<Message>(); }
     }
 
@@ -67,9 +119,22 @@ namespace DAV {
         mission: Mission
         status: MessageStatus
         domain: MessageDomain
-        respond(status: MessageStatus, payload: string) { }
+
+        respond(type: string, payload: any) { }
+    }
+
+    interface SendMessageParams {
+        recipients?: Array<ID>
     }
 
     export class Mission {
+        constructor(missionId: ID) { }
+
+        id: ID
+        needer: Identity
+
+        sendMessage(type: string, payload: any, params: SendMessageParams) { }
+        messages(): Rx.Subject<Message> { return new Rx.Subject<Message>(); }
+        finalizeMission(walletPrivateKey: string) { }
     }
 }
