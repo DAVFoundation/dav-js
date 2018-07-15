ffbd1b3968d5a8f6a48e1e2eef60f89d7dd1ba41
hai alaluf <haialaluf@hais-MacBook-Pro.local>
2018-07-15 15:04:05 +0300

    Create interface for Need (DD-212)

--- samples/common-definitions.ts
+++ samples/common-definitions.ts
@@ -1,2 +1,9 @@
+namespace Rx {
+    export class Subject<T> {
+      public subscribe(success: (t: T) => void, error?: (t: T) => void) { /**/ }
+      public filter(cb: (t: T) => boolean): Subject<T> { return this; }
+    }
+} // Dummy Definition
+  
 type ID = string;
 type BigInteger = string;
