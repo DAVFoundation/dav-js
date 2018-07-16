ea579c1525c1dc394da11c34e7139441dd70a65a
srfrnk <srfrnk@gmail.com>
2018-07-16 17:29:05 +0300

    fix: created Observable extension

--- src/common.ts
+++ src/common.ts
@@ -1,2 +1,9 @@
+import { Observable as RxObservable } from 'rxjs/Rx';
+
+export type TopicID = string;
 export type ID = string;
 export type BigInteger = string;
+
+export class Observable<T> extends RxObservable<T> {
+    public topic: TopicID;
+}
