c7bca379e3d9f5443298d8cbd376420a9d3fcd22
srfrnk <srfrnk@gmail.com>
2018-07-16 17:06:35 +0300

    fix: added real Rx

--- src/Identity.ts
+++ src/Identity.ts
@@ -1,4 +1,5 @@
-import { ID, Rx } from './common';
+import { Observable } from 'rxjs/Rx';
+import { ID } from './common';
 import IConfig from './IConfig';
 import NeedParams from './NeedParams';
 import NeedFilterParams from './NeedFilterParams';
@@ -10,19 +11,19 @@ import Mission from './Mission';
 
 export default class Identity {
   public davId: ID;
-  private _messages: Rx.Observable<Message>;
+  private _messages: Observable<Message>;
 
   constructor(davId: ID, privateKey: string, private config: IConfig) {/**/ }
 
-  public needsForType(params: NeedFilterParams): Rx.Observable<Need> { return new Rx.Observable<Need>(); }
+  public needsForType(params: NeedFilterParams): Observable<Need> { return new Observable<Need>(); }
   public need(needId: ID): Need { return new Need(needId, this.config); }
   public bid(bidId: ID): Bid { return new Bid(bidId, this.config); }
   public mission(missionId: ID): Mission { return new Mission(missionId, this.config); }
-  public messages(): Rx.Observable<Message> {
+  public messages(): Observable<Message> {
     if (!this._messages) {
-      this._messages = new Rx.Observable<Message>();
+      this._messages = new Observable<Message>();
     }
     return this._messages;
   }
   public publishNeed(params: NeedParams): Need { return new Need(params, this.config); }
-  }
+}
--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,2 @@
-export namespace Rx {
-    export class Observable<T> {
-      public subscribe(success: (t: T) => void, error?: (t: T) => void) { /**/ }
-      public filter(cb: (t: T) => boolean): Observable<T> { return this; }
-    }
-} // Dummy Definition
 export type ID = string;
 export type BigInteger = string;
