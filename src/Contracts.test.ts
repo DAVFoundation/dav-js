1e98e56b1bc6ca1fe28777d8fa8483213238e14a
Hai <hai.alaluf@gmail.com>
2018-07-22 16:31:35 +0300

    implement isIdentityRegistered: small fix for commit(DD-231)

--- src/Contracts.test.ts
+++ src/Contracts.test.ts
@@ -10,11 +10,11 @@ describe('Contracts class', () => {
   describe('isRegistered method', () => {
     beforeAll(() => { /**/ });
 
-    it('should return false for unregistered Id', async () => {
+    it('should return true for registered Id', async () => {
         expect(Contracts.isIdentityRegistered('0xFEDdDcBf94cB620d6D92D049b75fc7062a3E2Fc6', configuration)).resolves.toEqual(true);
     });
 
-    it('should return false for registered Id', async () => {
+    it('should return false for unregistered Id', async () => {
         expect(Contracts.isIdentityRegistered('0xCE71a46b6837e474def0c1B244868e8b7307F51b', configuration)).resolves.toEqual(false);
     });
 
