1d0e540f85a1b2f12565964fdf311b694fdcc57e
yaronsha <yaron.shapira7@gmail.com>
2018-07-22 11:31:44 +0300

    fix: add needer identity to mission c'tor. remove private key from getIdentity in SDK. DD-235

--- src/Bid.ts
+++ src/Bid.ts
@@ -3,6 +3,7 @@ import IConfig from './IConfig';
 import BidParams from './BidParams';
 import Message from './Message';
 import Mission from './Mission';
+import Identity from './Identity';
 
 export default class Bid<T extends BidParams> {
     get params(): T {
@@ -16,7 +17,7 @@ export default class Bid<T extends BidParams> {
     public async accept() { /**/ }
     public async signContract(walletPrivateKey: string): Promise<Mission> {
         return new Mission(''/* new topic */,
-            undefined /* no peer yet */, this.config);
+            undefined /* no peer yet */, new Identity('id', 'davId', this.config), this.config);
     }
     public messages(): Observable<Message> { return new Observable<Message>(); }
 }
