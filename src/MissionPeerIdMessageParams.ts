bdbcb7829b2a98edf21a5e3f9d95fe66b5936d4e
Hai <hai.alaluf@gmail.com>
2018-09-03 16:57:28 +0300

    change file name

--- src/MissionPeerIdMessageParams.ts
+++ src/MissionPeerIdMessageParams.ts
@@ -3,21 +3,21 @@ import BaseMessageParams from './MessageParams';
 /**
  * @class The Class MessageParams represent common parameters of MessageParams classes.
  */
-export default class GeneralMessageParams extends BaseMessageParams {
+export default class MissionPeerIdMessageParams extends BaseMessageParams {
 
     private static _protocol = 'general';
     private static _type = 'message';
 
     public static getMessageType(): string {
-        return GeneralMessageParams._type;
+        return MissionPeerIdMessageParams._type;
     }
 
     public static getMessageProtocol(): string {
-        return GeneralMessageParams._protocol;
+        return MissionPeerIdMessageParams._protocol;
     }
 
-    constructor(values?: Partial<GeneralMessageParams>) {
-        super(GeneralMessageParams._protocol, GeneralMessageParams._type, values);
+    constructor(values?: Partial<MissionPeerIdMessageParams>) {
+        super(MissionPeerIdMessageParams._protocol, MissionPeerIdMessageParams._type, values);
     }
 
     public serialize() {
