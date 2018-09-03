75de1ae83b891df45ab55cf6e5bf1299dd1da9b4
Hai <hai.alaluf@gmail.com>
2018-09-03 16:54:48 +0300

    resolve merge conflict

--- src/BasicParams.ts
+++ src/BasicParams.ts
@@ -13,15 +13,11 @@ export default abstract class BasicParams {
         throw new Error('Must be implemented by derived class');
     }
 
-<<<<<<< HEAD
     public static getMessageProtocol(): string {
         throw new Error('Must be implemented by derived class');
     }
 
-    public constructor(values: Partial<IBasicParams>, private _protocol: string, private _type: string) {
-=======
     public constructor(private _protocol: string, private _type: string, values?: Partial<IBasicParams>) {
->>>>>>> 6396c3ed09d258b6e913b2e192c2b83d04e27fbf
         if (!!values) {
             this.ttl = values.ttl;
         }
@@ -35,11 +31,9 @@ export default abstract class BasicParams {
         };
     }
 
-<<<<<<< HEAD
     public abstract getProtocolTypes(): any;
-=======
+
     public deserialize(json: any): void {
         this.ttl = json.ttl;
     }
->>>>>>> 6396c3ed09d258b6e913b2e192c2b83d04e27fbf
 }
