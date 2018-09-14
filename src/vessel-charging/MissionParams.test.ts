55e88bd98b9330aa732f450fb77194f08c014eab
Tal Ater <tal@talater.com>
2018-09-14 18:45:47 +0300

    Tweaked formatting

--- src/vessel-charging/MissionParams.test.ts
+++ src/vessel-charging/MissionParams.test.ts
@@ -1,34 +1,34 @@
 import MissionParams from './MissionParams';
 
 describe('MissionParams class', () => {
-    const missionParams = new MissionParams({
-        id: 'TOPIC_ID',
-        neederDavId: 'davId',
-        price: '3',
-        vehicleId: 'vehicleId',
-    });
+  const missionParams = new MissionParams({
+    id: 'TOPIC_ID',
+    neederDavId: 'davId',
+    price: '3',
+    vehicleId: 'vehicleId',
+  });
 
-    const serializedMissionParams: any = {
-        ttl: undefined,
-        protocol: 'boat_charging',
-        type: 'mission',
-        id: 'TOPIC_ID',
-        neederDavId: 'davId',
-        price: ['3'],
-        vehicleId: 'vehicleId',
-    };
+  const serializedMissionParams: any = {
+    ttl: undefined,
+    protocol: 'boat_charging',
+    type: 'mission',
+    id: 'TOPIC_ID',
+    neederDavId: 'davId',
+    price: ['3'],
+    vehicleId: 'vehicleId',
+  };
 
-    describe('serialize method', () => {
-        it('should return serialized mission params object with the current values', () => {
-            expect(missionParams.serialize()).toEqual(serializedMissionParams);
-        });
+  describe('serialize method', () => {
+    it('should return serialized mission params object with the current values', () => {
+      expect(missionParams.serialize()).toEqual(serializedMissionParams);
     });
+  });
 
-    describe('deserialize method', () => {
-        it('should return MissionParams instance with the current parameters', () => {
-            const missionParamsObject = new MissionParams();
-            missionParamsObject.deserialize(serializedMissionParams);
-            expect(missionParamsObject).toEqual(missionParams);
-        });
+  describe('deserialize method', () => {
+    it('should return MissionParams instance with the current parameters', () => {
+      const missionParamsObject = new MissionParams();
+      missionParamsObject.deserialize(serializedMissionParams);
+      expect(missionParamsObject).toEqual(missionParams);
     });
+  });
 });
