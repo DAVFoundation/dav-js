cf69616e544999a520b3f1b5c9fd26a9b3bbb2ce
Tal Ater <tal@talater.com>
2018-09-14 10:16:31 +0300

    Formatted MissionParams test file

--- src/ride-hailing/MissionParams.test.ts
+++ src/ride-hailing/MissionParams.test.ts
@@ -1,28 +1,31 @@
 import MissionParams from './MissionParams';
 
 describe('MissionParams class', () => {
- const missionParams = new MissionParams({});
- missionParams.id = 'TOPIC_ID';
- missionParams.neederDavId = 'DavId';
- const serializedMissionParams: any = {
-  id: 'TOPIC_ID',
-  neederDavId: 'DavId',
-  ttl: undefined,
-  protocol: 'ride_hailing',
-  type: 'mission',
-  price: undefined,
-  vehicleId: undefined,
- };
- describe('serialize method', () => {
-  it('should return serialized need params object with the current values', () => {
-    expect(missionParams.serialize()).toEqual(serializedMissionParams);
+
+  const missionParams = new MissionParams({});
+  missionParams.id = 'TOPIC_ID';
+  missionParams.neederDavId = 'DavId';
+  const serializedMissionParams: any = {
+    id: 'TOPIC_ID',
+    neederDavId: 'DavId',
+    ttl: undefined,
+    protocol: 'ride_hailing',
+    type: 'mission',
+    price: undefined,
+    vehicleId: undefined,
+  };
+
+  describe('serialize method', () => {
+    it('should return serialized need params object with the current values', () => {
+      expect(missionParams.serialize()).toEqual(serializedMissionParams);
+    });
   });
- });
- describe('deserialize method', () => {
-  it('should return NeedParams instance with the current parameters', () => {
-   const missionParamsObject = new MissionParams();
-   missionParamsObject.deserialize(serializedMissionParams);
-   expect(missionParamsObject).toEqual(missionParams);
+
+  describe('deserialize method', () => {
+    it('should return NeedParams instance with the current parameters', () => {
+      const missionParamsObject = new MissionParams();
+      missionParamsObject.deserialize(serializedMissionParams);
+      expect(missionParamsObject).toEqual(missionParams);
+    });
   });
- });
 });
