4d23cdeecdb3a77f3c331f981e947fce59088a68
srfrnk <srfrnk@gmail.com>
2018-09-28 07:44:12 +0300

    fix: DD-325 fixed

--- dist/sdkLogger.js
+++ dist/sdkLogger.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function sdkLogger(message, ...optionalParams) {
-    // tslint:disable-next-line:no-console
-    console.log(message, optionalParams);
+    const sdkDebugLog = process.env.SDK_DEBUG_LOG === 'true';
+    if (sdkDebugLog) {
+        // tslint:disable-next-line:no-console
+        console.log(message, optionalParams);
+    }
 }
 exports.default = sdkLogger;
 
