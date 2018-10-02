e3d578cb63313364d0648a501996c77074903f8c
Tal Ater <tal@talater.com>
2018-10-02 18:15:36 +0300

    chore: update dist files

--- dist/ride-hailing/BidParams.d.ts
+++ dist/ride-hailing/BidParams.d.ts
@@ -1,5 +1,6 @@
 import BaseBidParams from '../BidParams';
 import IBaseBidParams from '../IBidParams';
+import { ILocation } from '../common-types';
 /**
  * @interface IVehicleDetails is represents vehicle details in a bid of ride-hailing protocol
  */
@@ -32,7 +33,7 @@ interface IBidParams extends IBaseBidParams {
     /**
      * @property The provider's vehicle current location (required).
      */
-    currentVehicleLocation: Location;
+    currentVehicleLocation: ILocation;
     /**
      * @property The vehicle details.
      */
@@ -51,7 +52,7 @@ export default class BidParams extends BaseBidParams {
     /**
      * @property The provider's vehicle current location.
      */
-    currentVehicleLocation: Location;
+    currentVehicleLocation: ILocation;
     /**
      * @property The vehicle details.
      */
