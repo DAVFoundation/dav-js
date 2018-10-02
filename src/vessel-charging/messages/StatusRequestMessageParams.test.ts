0b6d081dea03ee63a83ba12dbd730ad840d46101
Jongerr <jontmichie@gmail.com>
2018-10-02 12:35:13 -0500

    Readd failing test and add protocol and ttl to serialized object.

--- src/vessel-charging/messages/StatusRequestMessageParams.test.ts
+++ src/vessel-charging/messages/StatusRequestMessageParams.test.ts
@@ -3,12 +3,14 @@ import MessageParams from './StatusRequestMessageParams';
 describe('MessageParams class', () => {
   const messageParams = new MessageParams({ senderId: 'senderId' }); // <-- Causing TypeError
   const serializedMessageParams: any = {
+    protocol: 'vessel_charging',
+    ttl: undefined,
     type: 'status_request_message',
     senderId: 'senderId',
   };
 
   describe('serialize method', () => {
-    xit('should return serialized message params object with the current values', () => {
+    it('should return serialized message params object with the current values', () => {
       expect(messageParams.serialize()).toEqual(serializedMessageParams);
     });
   });
