423091b5665c64ecdb224be87380245e62b11af6
Indraneel Sarkar <indraneel.sarkar96@gmail.com>
2018-10-05 21:17:47 +0530

    Update tests for src/ride-hailing/MissionParams.test.ts using Jest

--- src/ride-hailing/MissionParams.test.ts
+++ src/ride-hailing/MissionParams.test.ts
@@ -23,13 +23,12 @@ describe('MissionParams class', () => {
   });
 
   describe('deserialize method', () => {
-  	// testing for instance
     it('should return serialized MissionParams instance with the current parameters', () => {
       const missionParamsObject = new MissionParams();
       missionParamsObject.deserialize(serializedMissionParams);
       expect(missionParamsObject).toBeInstanceOf(MissionParams);
     });
-    // testing for object content
+    
     it('should return serialized MissionParams object with the current parameters', () => {
       const missionParamsObject = new MissionParams();
       missionParamsObject.deserialize(serializedMissionParams);
