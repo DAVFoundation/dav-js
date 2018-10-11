addf79dfa0df64e3f342554baef3119364424682
mariolo1985 <loserpunkx182@gmail.com>
2018-10-10 19:27:45 -0600

    using node fs

--- preinstall.js
+++ preinstall.js
@@ -1,23 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
-function checkForFiles(err, files) {
-    console.log(files.length);
-    if ((err===null) && (files.length> 0)) {
-        // found files in the directory so we need to empty the directory
-        files.forEach(function (file) {
-            var filename = path.join('.githooks', file);
-            console.log(file);
-        });
-    }
+const srcDir = '.githooks';
+const destDir = '.git/hooks';
+
+function addGitHooks() {
+  const srcFiles = fs.readdirSync(srcDir);
+  srcFiles.forEach(function (file) {
+    var srcFilename = path.join(srcDir, file);
+    var DestFilename = path.join(destDir, file);
+    fs.copyFileSync(srcFilename, DestFilename);
+  });
 }
 
-function addGitHooks(err) {
-    if (err) {
-        console.log(err);
-    }
-    fs.copyFile('.githooks', '.git/hooks');
+function deleteDirectoryFiles(err, files) {
+  if ((err === null) && (files.length > 0)) {
+    // found files in the directory so we need to empty the directory
+    files.forEach(function (file) {
+      var filename = path.join(destDir, file);
+      fs.unlinkSync(filename);
+    });
+  }
+
+  // remove dest and copy from src
+  //fs.rmdir(destDir, addGitHooks);
+  addGitHooks();
 }
 
-fs.readdir('.git/hooks', checkForFiles);
-//fs.rmdir('.git/hooks', addGitHooks);
+// Start by cleaning dest
+fs.readdir(destDir, deleteDirectoryFiles);
