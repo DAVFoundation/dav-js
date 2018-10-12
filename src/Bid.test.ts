d018084380405a6afeddfd2e4e5526940f04279d
Steven <goingtogaming@gmail.com>
2018-10-12 23:44:58 +0200

    Removed unneeded code message params

--- src/Bid.test.ts
+++ src/Bid.test.ts
@@ -20,9 +20,7 @@ describe('Bid class', () => {
   const selfId = 'SELF_ID';
   jest.doMock('./ride-hailing/BidParams');
   let bidParams: BidParams;
-  let messageParams = new MessageParams({
-    senderId: 'sender',
-  });
+  let messageParams = MessageParams;
   const missionParams = new MissionParams({
     id: 'MISSION_ID',
     neederDavId: 'DAV_ID',
