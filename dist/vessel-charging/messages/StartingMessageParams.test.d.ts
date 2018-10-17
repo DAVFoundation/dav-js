11e0da17192ba7f7a6d5fd6f6692ba047e2aa8ba
Indraneel Sarkar <indraneel.sarkar96@gmail.com>
2018-10-17 17:39:43 +0530

    chore: update dist files

---
+++ dist/vessel-charging/messages/StartingMessageParams.test.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=StartingMessageParams.test.d.ts.map
---
+++ dist/vessel-charging/messages/StartingMessageParams.test.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const StartingMessageParams_1 = require("./StartingMessageParams");
+describe('StartingMessageParams class', () => {
+    let startingMessageParams;
+    let serializedMessageParams;
+    beforeEach(() => {
+        startingMessageParams = new StartingMessageParams_1.default({});
+        serializedMessageParams = {
+            ttl: undefined,
+            protocol: 'vessel_charging',
+            type: 'starting_message',
+            senderId: undefined,
+        };
+    });
+    describe('serialize method', () => {
+        it('should return serialized message params object with the current values', () => {
+            expect(startingMessageParams.serialize()).toEqual(serializedMessageParams);
+        });
+    });
+    describe('deserialize method', () => {
+        it('should return a MessageParams instance', () => {
+            const startingMessageObj = new StartingMessageParams_1.default();
+            startingMessageObj.deserialize(serializedMessageParams);
+            expect(startingMessageObj).toBeInstanceOf(StartingMessageParams_1.default);
+        });
+        it('should return a correct MessageParams object', () => {
+            const startingMessageObj = new StartingMessageParams_1.default({});
+            startingMessageObj.deserialize(serializedMessageParams);
+            expect(startingMessageObj).toEqual(startingMessageParams);
+        });
+    });
+});
+
+//# sourceMappingURL=StartingMessageParams.test.js.map
