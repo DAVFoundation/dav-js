8c40a9fdfdabc5f232e3acf102511c729b91ee0b
srfrnk <srfrnk@gmail.com>
2019-11-21 18:37:06 +0200

    fix

---
+++ dist/drone-charging/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const BidParams_1 = require("./BidParams");
+exports.BidParams = BidParams_1.default;
+const MessageParams_1 = require("./MessageParams");
+exports.MessageParams = MessageParams_1.default;
+const MissionParams_1 = require("./MissionParams");
+exports.MissionParams = MissionParams_1.default;
+const NeedFilterParams_1 = require("./NeedFilterParams");
+exports.NeedFilterParams = NeedFilterParams_1.default;
+const NeedParams_1 = require("./NeedParams");
+exports.NeedParams = NeedParams_1.default;
+const ProtocolTypes_1 = require("./ProtocolTypes");
+exports.ProtocolTypes = ProtocolTypes_1.default;
+
+//# sourceMappingURL=index.js.map
---
+++ src/drone-charging/index.ts
@@ -0,0 +1,15 @@
+import BidParams from './BidParams';
+import MessageParams from './MessageParams';
+import MissionParams from './MissionParams';
+import NeedFilterParams from './NeedFilterParams';
+import NeedParams from './NeedParams';
+import ProtocolTypes from './ProtocolTypes';
+
+export {
+  BidParams,
+  MessageParams,
+  MissionParams,
+  NeedFilterParams,
+  NeedParams,
+  ProtocolTypes,
+};
